4376f7d572d7dd16c1348cd93d70b6080098c0b8
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-06-24 10:14:05 +0200

    test changes

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -7,13 +7,17 @@ export default class PortfolioContainer extends Component {
         super();
         this.state = {
             pageTitle: "Welcome to my Portfolio",
+            isLoading: false,
             data: [
-                { tittle: "Quip" }, 
-                { tittle: "Eventbrite" },
-                { tittle: "Ministry Safe" },
-                { tittle: "SwingAway" }
+                { tittle: "Quip", category: "eCommerce" },
+                { tittle: "Eventbrite", category: "Scheduling" },
+                { tittle: "Ministry Safe", category: "EnterPrise" },
+                { tittle: "SwingAway", category: "eCommerce" }
             ]
         };
+
+        this.handleFilter = this.handleFilter.bind(this);   
+
     }
 
     portfolioItems() {
@@ -23,12 +27,28 @@ export default class PortfolioContainer extends Component {
         })
     }
 
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => item.category === filter)
+        })  
+    }
+
+
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>
+        }
+
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
 
+                <button onClick={() => this.handleFilter("eCommerce")}>eCommerce</button>
+                <button onClick={() => this.handleFilter("Scheduling")}>Scheduling</button>
+                <button onClick={() => this.handleFilter("EnterPrise")}>EnterPrise</button>
+
                 {this.portfolioItems()}
+
             </div>
         )
     }
