da1d42a862e76498401c04c98c93a49d4bb7558f
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-07-12 06:49:07 +0200

    forms added

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,11 +1,81 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          name: '',
+          description: '',
+          category: '',
+          position: '',
+          url: '',
+          thumb_image: '',
+          banner_image: '',
+          logo: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+
+    buildForm() {
+      let formData = new FormData();
+
+      formData.append('portfolio_item[name]', this.state.name);
+      formData.append('portfolio_item[url]', this.state.url);
+      formData.append('portfolio_item[category]', this.state.category);
+      formData.append('portfolio_item[description]', this.state.description);
+      formData.append('portfolio_item[position]', this.state.position);
+
+      return formData;
+    }
+
+    handleChange(event) {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+
+    handleSubmit(event) {
+      axios.post('https://rubensballester.devcamp.space/portfolio/portfolio_items', this.buildForm(), {
+        withCredentials: true
+      }).then(response => {
+        console.log("response", response);
+      }).catch(error => {
+        console.log("portfolio form handleSubmit error", error);
+      });
+
+
+      event.preventDefault();
+    }
+    
+
     render() {
       return ( 
         <div className="portfolio-form-wrapper">
+
           <h1>Portfolio Form</h1>
-          <h1>Portfolio Form</h1>
+
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <input type="text" name="name" placeholder="Portfolio Item Name" value={this.state.name} onChange={this.handleChange} />
+              <input type="text" name="url" placeholder="URL" value={this.state.url} onChange={this.handleChange} />
+            </div>
+            <div>
+              <input type="text" name="position" placeholder="Position" value={this.state.position} onChange={this.handleChange} />
+              <input type="text" name="category" placeholder="Category" value={this.state.category} onChange={this.handleChange} />
+            </div>
+            <div>
+              <input type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.handleChange} />
+            </div>
+
+            <div>
+              <button typu="submit">Save</button>
+            </div>
+          </form>
+          
         </div>
       )
     }
