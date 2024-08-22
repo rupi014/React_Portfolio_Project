ba374c316fa37ab1af5c48d541f19fa96fac524c
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-08-22 09:38:54 +0200

    blog component added

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
-export default function Blog() {
-  return ( 
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor() {
+    super();
 
-    <div>
-        <Link to="/about-me">Read More About Myself</Link>
-    </div>
-    </div>
-  );
+    this.state = {
+      blogItems: []
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+      axios.get("https://rubensballester.devcamp.space/portfolio/portfolio_blogs",
+        { withCredentials: true }
+      ).then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        })
+      }).catch(error => {
+        console.log("error in getBlogItems", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogrecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />
+    });
+    
+    return ( 
+      <div className='blog-container'>
+        <div className='content-container'>{blogrecords}</div>
+      </div>
+    );
+  }
 }
+
+export default Blog;
