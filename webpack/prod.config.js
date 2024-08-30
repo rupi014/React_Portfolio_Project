df3c5a3c90f4131a3bc392b3e2ca4090a386f5cc
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-08-30 18:25:38 +0200

    update

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,64 +1,81 @@
-const path = require("path");
-const webpackMerge = require("webpack-merge");
-const autoprefixer = require("autoprefixer");
-const webpackCommon = require("./common.config");
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+const autoprefixer = require('autoprefixer');
+const webpackCommon = require('./common.config');
 
 // webpack plugins
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const DefinePlugin = require("webpack/lib/DefinePlugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const LoaderOptionsPlugin = require("webpack/lib/LoaderOptionsPlugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
 
 module.exports = webpackMerge(webpackCommon, {
-  // ...
+
+  bail: true,
+
+  devtool: 'source-map',
+  mode: 'production',
+  output: {
+
+    path: path.resolve(__dirname, '../dist'),
+
+    filename: '[name]-[hash].min.js',
+
+    sourceMapFilename: '[name]-[hash].map',
+
+    chunkFilename: '[id]-[chunkhash].js',
+
+    publicPath: '/'
+  },
+
   module: {
+
     rules: [
       {
         test: /\.s?css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              importLoaders: 2
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                sourceMap: true,
+                importLoaders: 2
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                config: {
+                  path: path.resolve(__dirname, 'postcss.config.js')
+                },
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                outputStyle: 'expanded',
+                sourceMap: true,
+                sourceMapContents: true
+              }
             }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                config: path.resolve(__dirname, "postcss.config.js")
-              },
-              sourceMap: true
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true,
-            }
-          }
-        ]
+          ]
+        })
       }
     ]
+
   },
+
   plugins: [
-    // ...
-    new MiniCssExtractPlugin({
-      filename: "[name]-[chunkhash].min.css",
-    }),
-    // ...
-  ]
-});
     new HtmlWebpackPlugin({
       inject: true,
-      template: path.resolve(__dirname, "../static/index.html"),
-      favicon: path.resolve(__dirname, "../static/favicon.ico"),
+      template: path.resolve(__dirname, '../static/index.html'),
+      favicon: path.resolve(__dirname, '../static/favicon.ico'),
       minify: {
         removeComments: true,
         collapseWhitespace: true,
@@ -72,19 +89,21 @@ module.exports = webpackMerge(webpackCommon, {
         minifyURLs: true
       }
     }),
-    new CopyWebpackPlugin([{ from: path.resolve(__dirname, "../static") }], {
-      ignore: ["index.html", "favicon.ico"]
+    new CopyWebpackPlugin([
+      {from: path.resolve(__dirname, '../static')}
+    ], {
+      ignore: ['index.html', 'favicon.ico']
     }),
-    new CleanWebpackPlugin(["dist"], {
-      root: path.resolve(__dirname, ".."),
-      exclude: ".gitignore"
+    new CleanWebpackPlugin(['dist'], {
+      root: path.resolve(__dirname, '..'),
+      exclude: '.gitignore'
     }),
     new DefinePlugin({
-      "process.env": {
+      'process.env': {
         NODE_ENV: '"production"'
       }
     }),
-    new ExtractTextPlugin("[name]-[chunkhash].min.css"),
+    new ExtractTextPlugin('[name]-[chunkhash].min.css'),
     new UglifyJsPlugin({
       uglifyOptions: {
         compress: {
@@ -103,9 +122,12 @@ module.exports = webpackMerge(webpackCommon, {
     }),
     new LoaderOptionsPlugin({
       options: {
-        context: "/",
+        context: '/',
         sassLoader: {
-          includePaths: [path.resolve(__dirname, "../src")]
+          includePaths: [path.resolve(__dirname, '../src')]
         }
       }
     })
+  ]
+
+});
